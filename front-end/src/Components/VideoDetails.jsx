3a68802d1cce31eafff962e2cfc36264a7eef797
Nicole Slater <nicoleslater@pursuit.org>
2024-02-24 17:55:37 -0500

    Setting up file for Video Details

---
+++ front-end/src/Components/VideoDetails.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function VideoDetails() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default VideoDetails
--- front-end/src/Pages/Show.jsx
+++ front-end/src/Pages/Show.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from 'react';
+import VideoDetails from "../Components/VideoDetails";
 
 function Show() {
   return (
     <div>
-      
+      <VideoDetails />
     </div>
   )
 }
