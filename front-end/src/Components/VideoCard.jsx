6883f66a8f44d3ec360ca3a9edff941d86698485
Nicole Slater <nicoleslater@pursuit.org>
2024-03-03 15:59:17 -0500

    Updating Video Card Component with CSS

--- front-end/src/Components/VideoCard.jsx
+++ front-end/src/Components/VideoCard.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from 'react';
+import './VideoCard.css';
 
-function VideoCard() {
+const VideoCard = ({ title, thumbnailUrl, duration, views }) => {
   return (
-    <div>
-      
+    <div className="video-card">
+      <img className="thumbnail" src={thumbnailUrl} alt={title} />
+      <div className="video-info">
+        <h2 className="title">{title}</h2>
+        <div className="metadata">
+          <span className="duration">{duration}</span>
+          <span className="views">{views} views</span>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default VideoCard;
+
