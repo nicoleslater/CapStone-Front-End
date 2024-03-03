2c3346eb2484264cc2499445a2858ecb700f10f2
Nicole Slater <nicoleslater@pursuit.org>
2024-03-03 15:48:50 -0500

    Adding Modal and questionaire as per wireframe for User Flow

--- front-end/src/Components/Questionaire.jsx
+++ front-end/src/Components/Questionaire.jsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 
 function Questionaire() {
+  const [selectedInterests, setSelectedInterests] = useState([]);
+
+ 
   return (
     <div>
       
