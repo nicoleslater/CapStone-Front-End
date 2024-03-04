7636b997b8721690ed5fc13010ed3a03b711b6ca
Nicole Slater <nicoleslater@pursuit.org>
2024-03-04 15:44:55 -0500

    Updating Profile Page CSS

--- front-end/src/Components/Profile.jsx
+++ front-end/src/Components/Profile.jsx
@@ -1,11 +1,47 @@
 import React from 'react';
+import './Profile.css';
+
 
 function Profile() {
   return (
-    <div>
-      
+    <div className="profile-container">
+    <div className="sidebar">
+      <div className="sidebar-icon">Broadcast</div>
+      <div className="sidebar-icon">Index</div>
+      <div className="sidebar-icon">Settings</div>
     </div>
-  )
+    <div className="main-content">
+      <div className="top-section">
+        <div className="logo">
+          <img src="logo-image.jpg" alt="Your Logo" />
+        </div>
+        <div className="profile-info">
+          <div className="profile-picture">
+            <img src="profile-picture.jpg" alt="Profile" />
+          </div>
+          <div className="profile-name">Your Name</div>
+        </div>
+      </div>
+      <div className="profile-details">
+        <div>Location:</div>
+        <div>Skills:</div>
+        <div>Interests:</div>
+        <button>Edit Profile</button>
+      </div>
+      <div className="tidbits">
+        <h2>Your Uploaded Tidbits</h2>
+        {/* Video cards */}
+      </div>
+      <div className="tidbits">
+        <h2>Saved Tidbits</h2>
+        {/* Video cards */}
+      </div>
+    </div>
+    <div className="message-button">Message</div>
+  </div>
+  
+  );
 }
+
 
 export default Profile;
