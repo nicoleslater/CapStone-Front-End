f507a746560bb69024adc30922119c4381059bc4
Nicole Slater <nicoleslater@pursuit.org>
2024-03-04 04:45:33 -0500

    Updated NewForm.jsx with useParams import

--- front-end/src/Components/VideoNewForm.jsx
+++ front-end/src/Components/VideoNewForm.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams }  from "react-router-dom";
 
 const API = import.meta.env.VITE_BASE_URL;
 
