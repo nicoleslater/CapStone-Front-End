08481b4b924ba1a9ec2da06e9d58208e3d424884
Nicole Slater <nicoleslater@pursuit.org>
2024-03-03 20:17:57 -0500

    Updating Home Page, added guitar image, will continue to add elements

--- front-end/src/Pages/Home.jsx
+++ front-end/src/Pages/Home.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
-import "./Home.css";
+import './Home.css';
+import guitar from '../images/guitar.jpeg';
 
 function Home() {
   return (
-    <div>
-      
+    <div className="home-container">
+      <div className="image-container">
+        <img src={guitar} alt="guitar" className="image" />
+      </div>
+      <div className="text-container">
+      <h2> Where Those Who Know</h2>
+      <h2>Go to Grow</h2>
+      <p>Embark on a journey where expertise meets evolution! Welcome to our dynamic platform, your destination to thrive and grow. Join the league of knowledge-seekers and experience the power of growth firsthand.</p>
+      <button>Start Learning</button>
+    </div>
     </div>
   )
 }
