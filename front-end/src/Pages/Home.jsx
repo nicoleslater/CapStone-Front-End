d9794dd92576413bb69a5f59d9097c0186dabfd4
Nicole Slater <nicoleslater@pursuit.org>
2024-03-07 13:41:10 -0500

    Updating Home Page Component, changed Home Image Picture

--- front-end/src/Pages/Home.jsx
+++ front-end/src/Pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Home.css';
-import guitar from '../images/guitar.jpeg';
+import image from'../images/image.jpeg';
 import Tidbit from '../images/Tidbit.png';
 
 function Home() {
@@ -8,11 +8,11 @@ function Home() {
     <div className="home-container">
       <img src={Tidbit} alt="Tidbit Logo" className="tidbit-logo" />
       <div className="image-container">
-        <img src={guitar} alt="guitar" className="image" />
+        <img src={image} alt="image" className="image" />
       </div>
       <div>
       <h2 className="home-header-title"> Where Those Who Know</h2>
-      <h2 lassName="home-header-subtitle">Go to Grow</h2>
+      <h2 className="home-header-subtitle">Go to Grow</h2>
       <p>Explore clear, concise insights across any subject—from tech to art, math to fashion. Our site demystifies complex concepts and abstract ideas, making them accessible to all. Dive into our straightforward guides and unlock understanding in areas where clarity meets simplicity.</p>
       <button className="start-learning-button">Start Learning</button>
     </div>
