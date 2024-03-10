e721f6f118bdf1ac9c1a4526260db9bc62a46fbe
Michael Caldwell <michaelcaldwell@pursuit.org>
2024-03-10 12:23:00 -0400

    Update VideoNewForm/VideoEditForm

--- front-end/src/Components/VideoEditForm.jsx
+++ front-end/src/Components/VideoEditForm.jsx
@@ -1,32 +1,102 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import { Link, useNavigate, useParams } from 'react-router-dom'
 
-function VideoEditForm() {
+const API = import.meta.env.VITE_API_URL;
+
+function VideoEditForm ()
+{
+	const {id} = useParams()
+	const navigate = useNavigate();
+	const [video, setVideo] = useState({
+		src: "",
+		category: "",
+		topic: "",
+	} );
+	
+	const handleSourceChange = (e) => {
+		setVideo({ ...video, src: e.target.value });
+	};
+
+	const handleCategoryChange = (e) => {
+		setVideo({ ...video, category: e.target.value });
+	};
+
+	const handleTopicChange = (e) => {
+		setVideo({ ...video, topic: e.target.value });
+	};
+
+	useEffect(() => {
+		fetch(`${API}/videos/${id}`)
+			.then( ( response ) => response.json )
+			.then( ( video ) => setVideo( video ) )
+			.catch((error) => console.error(error))
+	}, [ id ] );
+	
+		const httpOptions = {
+			method: "PUT",
+			body: JSON.stringify(video),
+			headers: {
+				"Content-Type": "application/json",
+			},
+		};
+
+	const updateVideo = () => {
+		console.log(video);
+
+		fetch(`${API}/videos/${id}`, httpOptions)
+			.then(() => {
+				navigate(`/videos/${id}`);
+			})
+			.catch((error) => console.error(error));
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		updateVideo();
+	};
+
   return (
-			<form>
-				<label htmlFor='upload-video'>Source Location</label>
-				<br />
-				<input type='file' name='upload-video' id='upload-video' />
-				<br />
-				<label htmlFor='category'>Category</label>
-				<select name='category' id='category'>
-					<option value=''>------</option>
-					<option value='astrology'>Astrology</option>
-					<option value='economics'>Economics</option>
-					<option value='fried-cooking'>Fried Cooking</option>
-					<option value='gaming'>Gaming</option>
-					<option value='gardening'>Gardening</option>
-					<option value='hunting'>Hunting</option>
-					<option value='latin-dance'>Latin Dance</option>
-					<option value='literature'>Literature</option>
-					<option value='painting'>Painting</option>
-					<option value='pastries'>Pastries</option>
-					<option value='tech'>Tech</option>
-					<option value='wood-work'>Wood Work</option>
-				</select>
-				<br />
-				<label htmlFor='topic'>Topic</label>
-				<input type='text' name='topic' id='topic' />
-			</form>
+		<form onSubmit={handleSubmit}>
+			<label htmlFor='upload-video'>Source Location</label>
+			<br />
+			<input
+				type='file'
+				name='upload-video'
+				id='upload-video'
+				value={video.src}
+				onChange={handleSourceChange}
+			/>
+			<br />
+			<label htmlFor='category'>Category</label>
+			<select
+				name='category'
+				id='category'
+				value={video.category}
+				onChange={handleCategoryChange}>
+				<option value=''>------</option>
+				<option value='astrology'>Astrology</option>
+				<option value='economics'>Economics</option>
+				<option value='fried-cooking'>Fried Cooking</option>
+				<option value='gaming'>Gaming</option>
+				<option value='gardening'>Gardening</option>
+				<option value='hunting'>Hunting</option>
+				<option value='latin-dance'>Latin Dance</option>
+				<option value='literature'>Literature</option>
+				<option value='painting'>Painting</option>
+				<option value='pastries'>Pastries</option>
+				<option value='tech'>Tech</option>
+				<option value='wood-work'>Wood Work</option>
+			</select>
+			<br />
+			<label htmlFor='topic'>Topic</label>
+			<input
+				type='text'
+				name='topic'
+				id='topic'
+				value={video.topic}
+				onChange={handleTopicChange}
+			/>
+		</form>
 	);
 }
 
--- front-end/src/Components/VideoNewForm.jsx
+++ front-end/src/Components/VideoNewForm.jsx
@@ -1,14 +1,63 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+	
 
-function VideoNewForm() {
+const API = import.meta.env.VITE_API_URL;
+
+function VideoNewForm ()
+{
+	const navigate = useNavigate();
+	const [video, setVideo] = useState({
+		src: "",
+		category: "",
+		topic: "",
+	});
+
+	const httpOptions = {
+		method: "POST",
+		body: JSON.stringify(video),
+		headers: {
+			"Content-Type": "application/json",
+		},
+	};
+
+	const addVideo = () => {
+		console.log(video);
+
+		fetch(`${API}/upload`, httpOptions)
+			.then((response) => {
+				alert(`${video.topic} was added to the database!`);
+				navigate(`/videos`);
+			})
+			.catch((error) => console.error(error));
+	};
+
+	const handleSourceChange = (e) => {
+		setVideo({ ...video, src: e.target.value });
+	};
+
+	const handleCategoryChange = (e) => {
+		setVideo({ ...video, category: e.target.value });
+	};
+
+	const handleTopicChange = (e) => {
+		setVideo({ ...video, topic: e.target.value });
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log(video);
+		addVideo();
+	};
+
   return (
-		<form>
+		<form onSubmit={handleSubmit}>
 			<label htmlFor='upload-video'>Source Location</label>
 			<br />
-			<input type='file' name='upload-video' id='upload-video' />
+			<input type='file' name='upload-video' id='upload-video' value={video.src} onChange={handleSourceChange}/>
 			<br />
 			<label htmlFor='category'>Category</label>
-			<select name='category' id='category'>
+			<select name='category' id='category' value={video.category} onChange={handleCategoryChange}>
 				<option value=''>------</option>
 				<option value='astrology'>Astrology</option>
 				<option value='economics'>Economics</option>
@@ -25,7 +74,7 @@ function VideoNewForm() {
 			</select>
 			<br />
 			<label htmlFor='topic'>Topic</label>
-			<input type='text' name='topic' id='topic' />
+			<input type='text' name='topic' id='topic' value={video.topic} onChange={handleTopicChange} />
 		</form>
 	);
 }
